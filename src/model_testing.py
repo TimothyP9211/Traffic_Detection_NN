2344238a3d10eaeaa77f9ffafc12f88885180015
TimothyP9211 <timothyp9211@gmail.com>
2026-03-10 16:17:12 -0700

    update detection testing script section

--- src/model_testing.py
+++ src/model_testing.py
@@ -2,7 +2,8 @@ import csv
 from os import listdir
 from ultralytics import YOLO
 
-CLASS_NUM = 1
+CLASS_NUM = 51
+DETECT_NUM = 1
 
 # ================== #
 # TESTING CLASSIFIER 
@@ -18,20 +19,37 @@ with open("classify_dataset/ds1_labels.csv", newline='', encoding='utf8') as f:
 # test the classification model on a sample image
 final_classifier = YOLO("runs/classify/train/weights/best.pt")
 
+total_predictions = 0
 data_set = []
 for sign_class in range(CLASS_NUM):
     test_signs = listdir("./data/TEST/{}".format(sign_class))
     for sign_file in test_signs:
-        path = "./data/TEST/{}/{}".format(sign_class,sign_file)
-        data_set.append(path)
+        path = "./data/TEST/{}/{}".format(sign_class, sign_file)
+        data_set.append((path, sign_class))
+        total_predictions += 1
 
-for curr_path in data_set:
+correct_predictions = 0
+incorrect_predictions = 0 
+for curr_path, true_class in data_set:
     results = final_classifier(curr_path)
     result = results[0]
     pred_idx = result.probs.top1
     pred_label = result.names[pred_idx]
-    print("Predicted:", id_to_name[pred_label])
-    print("Confidence:", float(result.probs.top1conf))
+    if pred_label == str(true_class):
+        print("Correct prediction")
+        correct_predictions += 1
+    else: 
+        print("Incorrect Prediction:", id_to_name[pred_label])
+        print("Confidence:", float(result.probs.top1conf))
+        print("True class:", id_to_name[str(true_class)])
+        incorrect_predictions += 1
+
+print("=====================================")
+print("Total predictions:", total_predictions)
+print("Correct predictions:", correct_predictions)
+print("Incorrect predictions:", incorrect_predictions)
+print("Accuracy:", correct_predictions / total_predictions if total_predictions > 0 else 0)
+print("=====================================")
 
 # ================ #
 # TESTING DETECTOR 
@@ -39,14 +57,21 @@ for curr_path in data_set:
 
 # test the detection model on a sample image
 final_detector = YOLO("runs/detect/train4/weights/best.pt")
-results = final_detector("gostraight_21_test_sign.png", conf=0.25)
-for i, r in enumerate(results):
-    print("Boxes:", r.boxes.xyxy)
-    print("Confidences:", r.boxes.conf)
-    print("Classes:", r.boxes.cls)
 
-    # draw bounding boxes on the image and save it
-    if i == 0:
-        file = r.save(filename=str("output_detect/detector_output.png"))
-        print("image saved to:", file)
+detect_dataset = []
+for sign_class in range(DETECT_NUM):
+    test_signs = listdir("./data/TEST/{}".format(sign_class))
+    for sign_file in test_signs:
+        path = "./data/TEST/{}/{}".format(sign_class, sign_file)
+        detect_dataset.append(path)
+
+n = 0
+for path in detect_dataset:
+    results = final_detector(path, conf=0.25)
+    for i, r in enumerate(results):
+        # draw bounding boxes on the image and save it
+        if i == 0:
+            file = r.save(filename=str("output_detect/" + str(n) + ".png"))
+            n += 1
+            print("image saved to:", file)
 
